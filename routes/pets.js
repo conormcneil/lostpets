597bcfd4090750a61ccbca8ed8e0f3d89d64720f
Lizzie Szoke <lizzieszoke@Lizzies-MacBook-Air.local>
2016-06-12 12:34:21 -0600

    Pet data displayed

--- routes/pets.js
+++ routes/pets.js
@@ -22,16 +22,67 @@ router.get('/found', function(req, res, next) {
 });
 
 router.get('/all', function(req, res, next) {
-  res.render('pets/all');
+  knex('pets').then(function(pets) {
+    console.log(pets);
+    // for(var i=0; i<pets.length; i++) {
+    //   console.log(pets[i]['name']);
+    //   console.log(pets[i]['name'].charAt(0).toUpperCase() + pets[i]['name'].slice(1));
+    // }
+    function capitalizeFirst(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    res.render('pets/all', { pets: pets, fs: { echo: capitalizeFirst }});
+  });
+});
+
+router.get('/browselost', function(req, res, next) {
+  knex('pets').where('isFound', 'false').then(function(pets) {
+    console.log(pets);
+    function capitalizeFirst(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    res.render('pets/browselost', { pets: pets, fs: { echo: capitalizeFirst }});
+  });
+});
+
+router.get('/browsefound', function(req, res, next) {
+  knex('pets').where('isFound', 'true').then(function(pets) {
+    console.log(pets);
+    function capitalizeFirst(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    res.render('pets/browsefound', { pets: pets, fs: { echo: capitalizeFirst }});
+  });
 });
 
 router.get('/add/lost', function(req, res, next) {
+  console.log(req.session.id);
   res.render('pets/reportlost');
 });
 
-router.post('/add-lost-pet', function(req, res, next) {
-  // knex('pets').insert(req.)
-})
+router.post('/add/lost', function(req, res, next) {
+  console.log(req.body);
+  knex('pets').insert(
+    {
+      name: req.body.name,
+      species: req.body.species,
+      location: req.body.location,
+      age: req.body.age,
+      description: req.body.description,
+      user_id: req.session.id,
+      image: req.body.image,
+      contact: req.body.contact,
+      date: req.body.date,
+    }
+  )
+  .then(function(data) {
+    res.redirect('pets/success');
+  })
+  .catch(function(err) {
+    console.log(err);
+    res.render('pets/reportlost', { error: "Something went wrong in submitting your form. Please try again" })
+  });
+});
 
 router.get('/add/found', function(req, res, next) {
   res.render('pets/reportfound');
