cdaf45359785f587c9f8c3e68471d9504e2c0630
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-06-13 17:09:49 -0600

    petId appears on addimage route

---
+++ public/javascripts/imageupload.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('#button').click(function(){
+    var image_url = window.localStorage.getItem('image_url');
+    console.log(image_url);
+    $('#imageInput').val(image_url);
+    // $('#idInput').val(id);
+  })
+})
--- routes/pets.js
+++ routes/pets.js
@@ -30,13 +30,12 @@ router.get('/found', function(req, res, next) {
 router.get('/all', function(req, res, next) {
   knex('pets').then(function(pets) {
     knex('users').fullOuterJoin('pets', 'pets.user_id', 'users.id').whereNot('pets.name', 'null').then(function(data) {
-      console.log(data);
       var petsAndUsers = data;
       res.render('pets/all', { petsAndUsers: petsAndUsers, fs: { echo: capitalizeFirst }});
     });
   })
   .catch(function(err) {
-    console.log(err);
+    res.send(err);
   });
 });
 
@@ -44,7 +43,6 @@ router.get('/browselost', function(req, res, next) {
   knex('pets').then(function(pets) {
     knex('users').fullOuterJoin('pets', 'pets.user_id', 'users.id').where('isFound', 'false').then(function(data) {
       var petsAndUsers = data;
-      console.log(petsAndUsers);
       res.render('pets/browselost', { petsAndUsers: petsAndUsers, fs: { echo: capitalizeFirst }});
     });
   });
@@ -53,7 +51,6 @@ router.get('/browselost', function(req, res, next) {
 router.get('/browsefound', function(req, res, next) {
   knex('pets').then(function(pets) {
     knex('users').fullOuterJoin('pets', 'pets.user_id', 'users.id').where('isFound', 'true').then(function(data) {
-      console.log(data);
       var petsAndUsers = data;
       res.render('pets/browsefound', { petsAndUsers: petsAndUsers, fs: { echo: capitalizeFirst }});
     });
@@ -61,16 +58,32 @@ router.get('/browsefound', function(req, res, next) {
 });
 
 router.get('/add/lost', function(req, res, next) {
-  console.log(req.body);
   res.render('pets/reportlost', {});
 });
 
 router.post('/add/lost', function(req, res, next) {
-  console.log(req.body);
-  // res.redirect('/pets/add/lost/addimage');
-  //Upload image to Cloudinary and get hosted URL back
-  knex('pets').insert(
-    {
+  if(req.body.species === 'other') {
+    knex('pets').insert({
+      name: req.body.name,
+      species: req.body.other,
+      location: req.body.location,
+      age: req.body.age,
+      description: req.body.description,
+      user_id: req.session.id,
+      image: req.body.image,
+      contact: req.body.contact,
+      date: req.body.date,
+      isFound: 'false'
+    })
+    .then(function(data) {
+      res.redirect('/lost/:id/addimage');
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.render('pets/reportfound', { error: "Something went wrong in submitting your form. Please try again." })
+    });
+  } else {
+    knex('pets').insert({
       name: req.body.name,
       species: req.body.species,
       location: req.body.location,
@@ -80,67 +93,46 @@ router.post('/add/lost', function(req, res, next) {
       contact: req.body.contact,
       date: req.body.date,
       isFound: 'false'
-    }
-  )
-  .then(function(data) {
-    console.log(req.params);
-    res.redirect('/pets/add/lost/addimage');
-  })
-  .catch(function(err) {
-    console.log(err);
-    res.render('pets/reportlost', { error: "Something went wrong in submitting your form. Please try again." })
-  });
-  if(req.body.species === 'other') {
-    console.log(req.body.other);
-    knex('pets').insert(
-        {
-          name: req.body.name,
-          species: req.body.other,
-          location: req.body.location,
-          age: req.body.age,
-          description: req.body.description,
-          user_id: req.session.id,
-          image: req.body.image,
-          contact: req.body.contact,
-          date: req.body.date,
-          isFound: 'false'
-        })
-        .then(function(data) {
-          res.redirect('pets/success-found');
-        })
-        .catch(function(err) {
-          console.log(err);
-          res.render('pets/reportfound', { error: "Something went wrong in submitting your form. Please try again." })
-        });
+    })
+    .returning('id')
+    .then(function(id) {
+      console.log("ID: ", id);
+      res.redirect('/pets/add/lost/' + id + '/addimage');
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.render('pets/reportlost', { error: "Something went wrong in submitting your form. Please try again." })
+    });
   }
-    else {
-      knex('pets').insert(
-          {
-            name: req.body.name,
-            species: req.body.species,
-            location: req.body.location,
-            age: req.body.age,
-            description: req.body.description,
-            user_id: req.session.id,
-            contact: req.body.contact,
-            date: req.body.date,
-            isFound: 'false'
-          })
-          .then(function(data) {
-            res.redirect('pets/success-lost');
-          })
-          .catch(function(err) {
-            console.log(err);
-            res.render('pets/reportlost', { error: "Something went wrong in submitting your form. Please try again." })
-          });
-    }
-
 });
-router.get('/add/lost/addimage', function(req, res, next){
-  res.render('pets/imageupload', {
-    request: req.body,
+router.get('/add/lost/:id/addimage', function(req, res, next){
+  console.log("GET: ", req.params.id);
+  knex('pets')
+  .where({
+    id: req.params.id
   })
-})
+  .returning('id')
+  .then(function(id){
+    // window.localStorage.setItem('petId', id);
+    res.locals.petId = id;
+    console.log(res.locals.petId);
+    res.render('pets/imageupload');
+  });
+});
+router.post('/add/lost/:id/addimage', function(req, res, next){
+  // console.log("POST: ", req.body.id);
+  // console.log("POST: " + req.body);
+  knex('pets')
+  .where({
+    id: req.params.id
+  })
+  .insert({
+    image: req.body.image
+  })
+  .then(function(){
+    res.render('success-lost', {})
+  })
+});
 
 router.get('/add/pets/success-lost', function(req, res, next) {
   res.render('pets/success-lost');
@@ -152,7 +144,6 @@ router.get('/add/found', function(req, res, next) {
 
 router.post('/add/found', function(req, res, next) {
   if(req.body.species === 'other') {
-    console.log(req.body.other);
     knex('pets').insert(
         {
           name: req.body.name,
@@ -170,7 +161,7 @@ router.post('/add/found', function(req, res, next) {
           res.redirect('pets/success-found');
         })
         .catch(function(err) {
-          console.log(err);
+          res.send(err);
           res.render('pets/reportfound', { error: "Something went wrong in submitting your form. Please try again." })
         });
   }
@@ -192,7 +183,7 @@ router.post('/add/found', function(req, res, next) {
             res.redirect('pets/success-found');
           })
           .catch(function(err) {
-            console.log(err);
+            res.send(err);
             res.render('pets/reportfound', { error: "Something went wrong in submitting your form. Please try again." })
           });
     }
