64d9d452d011d8ebf584b9cda6df2abdfcfbc2a6
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-06-15 13:10:18 -0600

    updated seed data for use with updated tables

--- seeds/add_pets_data.js
+++ seeds/add_pets_data.js
@@ -7,35 +7,74 @@ exports.seed = function(knex, Promise) {
         knex('pets').insert({
             name: 'Spot',
             species: 'dog',
-            location: 'close to home',
+            location: '80302',
             age: '3 months',
             description: 'The cutest nearsighted Yellow Lab you ever saw',
             user_id: 1,
             isFound: false,
+            date: '05-25-2016',
             image: 'https://res.cloudinary.com/dmuipy77o/image/upload/v1465920528/spot_test.jpg',
-            contact: 'Contact user/owner'
+            contact: '5202751232'
         }),
         knex('pets').insert({
             name: 'Fido',
             species: 'dog',
-            location: '28th & Canyon',
+            location: '80304',
             age: '7 months',
             description: 'Small, gold Yorkie with cape, allergic to kryptonite',
             user_id: 2,
             isFound: false,
+            date: '03-30-2016',
             image: 'http://res.cloudinary.com/dmuipy77o/image/upload/v1465920528/fido_test.jpg',
-            contact: 'Contact user/owner'
+            contact: '5202751232'
         }),
         knex('pets').insert({
             name: 'Rose',
             species: 'cat',
-            location: 'wherever cats live',
+            location: '80241',
             age: '19 years',
             description: 'Rose is a small black cat with brown stripes and white paws.',
             user_id: 3,
             isFound: true,
+            date: '12-30-2015',
             image: 'http://res.cloudinary.com/dmuipy77o/image/upload/v1465920528/rose_test.jpg',
-            contact: 'Boulder Humane Society'
+            contact: '5202751232'
+        }),
+        knex('pets').insert({
+            name: 'Hunter',
+            species: 'ferrret',
+            location: '90210',
+            age: '15 years old',
+            description: 'I lost Hunter when my neighbor left the garage door open at my house. He doesn\'t really like people so he won\'t be approachable. If you find him, please contact me so that I can come pick him up myself.',
+            user_id: 3,
+            isFound: false,
+            date: '12-30-2015',
+            image: 'https://res.cloudinary.com/dmuipy77o/image/upload/v1466017008/i5zrygtp4osydkoaw9wk.jpg',
+            contact: '5202751232'
+        }),
+        knex('pets').insert({
+            name: 'Tank',
+            species: 'dog',
+            location: '85712',
+            age: '10 months',
+            description: 'blue nose pit, he loves people, kids and chewy toys. he loves bacon and was wearing a brown leather collar with his tags',
+            user_id: 2,
+            isFound: false,
+            date: '3-30-2016',
+            image: 'https://res.cloudinary.com/dmuipy77o/image/upload/v1466017454/n5igycm4vt8aspwc4ja4.jpg',
+            contact: '5202751232'
+        }),
+        knex('pets').insert({
+            name: 'Spike',
+            species: 'cat',
+            location: '80231',
+            age: '7 years old',
+            description: 'He has black spots on his tongue, loves hunting birds, was wearing a blue collar last time I saw him',
+            user_id: 1,
+            isFound: true,
+            date: '06-01-2016',
+            image: 'https://res.cloudinary.com/dmuipy77o/image/upload/v1466016668/dhqmu1dy1nagcsosozja.jpg',
+            contact: '5202751232'
         })
     );
 };
