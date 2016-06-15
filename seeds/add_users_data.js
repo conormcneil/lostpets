03f25bb5e7f351d1d8b0f6b3c4c1fde6a9966dfc
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-06-15 12:08:54 -0600

    quick commit

--- seeds/add_users_data.js
+++ seeds/add_users_data.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
         knex('users').insert({
             id: 1,
             username: 'andrew',
-            password: 'andrew',
+            password: '$2a$08$Fl.2PwybQk2M1LYOU2emcu74W/oMVapDWvGHLvPlt5DzOEtOvUS3W',
             email: 'andrew@andrew.com',
             image: 'examplehosting.com/andrew.jpg',
             first_name: 'andrew',
@@ -18,7 +18,7 @@ exports.seed = function(knex, Promise) {
         knex('users').insert({
             id: 2,
             username: 'lizzie',
-            password: 'lizzie',
+            password: '$2a$08$ak.2skitNrOT0C4/e9KP7ufu/ulv83aC7jGhehkAuRcbGz65quGDu',
             email: 'lizziebee422@gmail.com',
             image: 'examplehosting.com/lizzie.jpg',
             first_name: 'lizzie',
@@ -29,7 +29,7 @@ exports.seed = function(knex, Promise) {
         knex('users').insert({
             id: 3,
             username: 'conor',
-            password: 'conor',
+            password: '$2a$08$nz8VnQBycH/ggQhhZFUhJe0rKCETRqroqFsGD/BjxsEWEbGSFxOym',
             email: 'cmkingston21@gmail.com',
             image: 'examplehosting.com/conor.jpg',
             first_name: 'conor',
