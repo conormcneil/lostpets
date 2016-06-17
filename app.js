ba6a2acb205ceb4431e7c9b8fbe6c3294f710560
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-06-17 10:36:33 -0600

    another commit

--- app.js
+++ app.js
@@ -47,6 +47,12 @@ app.use(passport.session());
 app.use(cookieParser());
 // See express session docs for information on the options: https://github.com/expressjs/session
 app.use(session({ secret: process.env.AUTH0_CLIENT_SECRET, resave: false,  saveUninitialized: false }));
+
+// app.use(function(req,res) {
+//   res.locals.process = {
+//     env: process.env
+//   }
+// })
 // Auth0 callback handler
 app.get('/callback',
   passport.authenticate('auth0', { failureRedirect: '/error' }),
